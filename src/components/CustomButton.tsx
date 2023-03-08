63e4b7eb6bb822dc8533e30468d2bf83a272f654
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2023-03-07 21:00:30 -0600

    custom button

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,60 +1,73 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, TouchableNativeFeedback } from 'react-native';
 
-export const CustomButton = ( location:String , setCounter:Function, counter:number ) => {
+interface Props {
+    title: string;
+    position: 'br' | 'bl';
+    onPress: () => void;
+}
+
+export const CustomButton = ( {title, position, onPress}:Props ) => {
 
   return (
-    <TouchableOpacity
-            style={location == 'br' ? styles.fabLocationBR : styles.fabLocationBL}
-            onPress={() => {
-                setCounter(counter + 1)
-            }}
+    <>
+        <View
+            style={[styles.fabLocation, position === 'br' ? styles.rigth : styles.left]}
+        >
+        <TouchableNativeFeedback
+            // style={position === 'br' ? styles.fabLocationBR : styles.fabLocationBL} // Otra forma de hacerlo
+            onPress={ onPress }
+            background={TouchableNativeFeedback.Ripple('#28425B', false, 30)} // El tercer parametro es el radio del ripple
         >
             <View
                 style={styles.fab}
             >
                 <Text style = { styles.fabText}>
-                    {location == 'br' ? '+1' : '-1'}
+                    {title}
                 </Text>
             </View>
-        </TouchableOpacity>
+        </TouchableNativeFeedback>
+        </View>
+    </>
   )
 }
 
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        textAlign: 'center',
-    },
-    button: {
-        backgroundColor: 'red',
-        height: 50,
-        width: 50,
-        borderRadius: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }, 
-    fabLocationBR:{
+const styles = StyleSheet.create({ 
+    fabLocation:{
         position: 'absolute',
         bottom: 25,
+    },
+    rigth: {
         right: 25,
-
     },
-    fabLocationBL:{
-        position: 'absolute',
-        bottom: 25,
+    left: {
         left: 25,
     },
+    // fabLocationBR:{
+    //     position: 'absolute',
+    //     bottom: 25,
+    //     right: 25,
+
+    // },
+    // fabLocationBL:{
+    //     position: 'absolute',
+    //     bottom: 25,
+    //     left: 25,
+    // },
     fab: {
         backgroundColor: '#5856D6',
         width: 60,
         height: 60,
         borderRadius: 100,
         justifyContent: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 7,
+        },
+        shadowOpacity: 0.41,
+        shadowRadius: 9.11,
+        elevation: 8,
     },
     fabText: {
         color: 'white',
--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { CustomButton } from '../components/CustomButton';
 
 export const CounterScreen = () => {
@@ -9,7 +9,23 @@ export const CounterScreen = () => {
     <View style={styles.container}>
         <Text
         style={styles.title}>Counter: {counter}</Text>
-        <TouchableOpacity
+        <CustomButton 
+            title = '-1'
+            position = 'bl'
+            onPress = {() => setCounter(counter - 1)}
+        />
+
+        <CustomButton
+            title = '+1'
+            position = 'br'
+            onPress = {() => setCounter(counter + 1)}
+        />
+
+
+
+
+
+        {/* <TouchableOpacity
             style={styles.fabLocationBR}
             onPress={() => {
                 setCounter(counter + 1)
@@ -22,21 +38,8 @@ export const CounterScreen = () => {
                     +1
                 </Text>
             </View>
-        </TouchableOpacity>
-        <TouchableOpacity
-            style={styles.fabLocationBL}
-            onPress={() => {
-                setCounter(counter - 1)
-            }}
-        >
-            <View
-                style={styles.fab}
-            >
-                <Text style = { styles.fabText}>
-                    -1
-                </Text>
-            </View>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
+        
      
     </View>
   )
@@ -59,29 +62,5 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         justifyContent: 'center',
         alignItems: 'center',
-    }, 
-    fabLocationBR:{
-        position: 'absolute',
-        bottom: 25,
-        right: 25,
-
     },
-    fabLocationBL:{
-        position: 'absolute',
-        bottom: 25,
-        left: 25,
-    },
-    fab: {
-        backgroundColor: '#5856D6',
-        width: 60,
-        height: 60,
-        borderRadius: 100,
-        justifyContent: 'center',
-    },
-    fabText: {
-        color: 'white',
-        fontSize: 25,
-        fontWeight: 'bold',
-        alignSelf: 'center',
-    }
 })
